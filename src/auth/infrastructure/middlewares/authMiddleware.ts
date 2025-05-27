baad3bbbcca565c190367409e39cf782e9646f06
Mauricio <maurijavi7@gmail.com>
2025-05-27 21:30:26 +0200

    fix axios

--- src/auth/infrastructure/middlewares/authMiddleware.ts
+++ src/auth/infrastructure/middlewares/authMiddleware.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import axios from 'axios';
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction)=>{
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
+    // console.log(req.headers);
 
     if(!authHeader || !authHeader.startsWith('Bearer')){
         res.status(401).json({error: 'No token provided'});
@@ -10,10 +11,10 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
 
     const token = authHeader.split(' ')[1];
 
-    try{
-        const response = await axios.post('https://localhost:4000/auth/verify-token', { token });
-
-        if(response.data.valid) {
+    try {
+        const response = await axios.post('http://localhost:4000/auth/verify-token', { token });
+        console.log(response)
+        if (response.data.valid) {
             //locals es para compartir datos en las diferentes partes del middleware entre rutas, basicamente la url de postman que haces /employees-getAllEmployees en mitad de esas cosas va el middleware de verificación
             res.locals.user = {
                 id: response.data.user.id,
@@ -23,7 +24,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
         }else{
             res.status(401).json({error: 'Invalid token'})
         }
-    }catch{
+    } catch {
         res.status(401).json({error: 'Token verificacion failed'});
     };
 }; 
